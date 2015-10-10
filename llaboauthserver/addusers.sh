5693784ef926407ac0c0dd3b64b92f82b1780a4f
Gavin Gray <gavingray1729@gmail.com>
2015-10-10 12:44:58 +0100

    set up identity so we can stash later

--- llaboauthserver/addusers.sh
+++ llaboauthserver/addusers.sh
@@ -11,4 +11,7 @@ for line in `cat userlist`; do
   curl https://raw.githubusercontent.com/edinburghlivinglab/livinglab-hubserver/master/Extra%20Resources.ipynb > Extra-Resources.ipynb
   chown -R $user:users /home/$user
   chmod -R 700 /home/$user
+  cd /home/$user/dds-notebooks
+  git config user.email $USER@$HOSTNAME
+  git config user.name $USER
 done
