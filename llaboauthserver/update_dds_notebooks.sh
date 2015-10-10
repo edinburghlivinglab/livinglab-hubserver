3e73a4b6e57775a28582273ce6ad019cc8d7c1e0
Gavin Gray <gavingray1729@gmail.com>
2015-10-10 15:19:09 +0100

    new version of the script, using the script in dds-notebooks

--- llaboauthserver/update_dds_notebooks.sh
+++ llaboauthserver/update_dds_notebooks.sh
@@ -7,5 +7,5 @@ IFS="
 for line in `cat userlist`; do
   test -z "$line" && continue
   user=`echo $line | cut -f 1 -d' '`
-  sudo -u $user -H sh -c "cd /home/$user/dds-notebooks && git stash && git pull origin master"
+  sudo -u $user -H sh -c "cd /home/$user/dds-notebooks && python auto-update.py"
 done
