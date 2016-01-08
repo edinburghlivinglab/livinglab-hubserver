82b5c77fd88c984446b786670b5c0c2dedbdbe00
Gavin <g.d.b.gray@sms.ed.ac.uk>
2016-01-08 00:51:07 +0000

    fixes

--- llabjupyterhub-remote/user_init.sh
+++ llabjupyterhub-remote/user_init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # run as part of user authentication, assumes it is passed user name as first arg
-sudo -u $1 -H sh -c -x "cd /home/$1 && git config user.email '$1@$HOSTNAME' && git config user.name '$1' && git clone https://github.com/edinburghlivinglab/dds-notebooks.git && curl https://raw.githubusercontent.com/edinburghlivinglab/livinglab-hubserver/master/Extra%20Resources.ipynb > Extra-Resources.ipynb"
+sudo -u $1 -H sh -c -x "cd /home/$1 && git clone https://github.com/edinburghlivinglab/dds-notebooks.git && curl https://raw.githubusercontent.com/edinburghlivinglab/livinglab-hubserver/master/Extra%20Resources.ipynb > Extra-Resources.ipynb"
+sudo -u $1 -H sh -c -x "cd /home/$1/dds-notebooks && git config user.email '$1@$HOSTNAME' && git config user.name '$1'"
