8c9e9ba99596e8e19e83c89c73d446e90d86966c
Gavin <g.d.b.gray@sms.ed.ac.uk>
2016-01-09 15:18:20 +0000

    include installing extra requirements in the build process

--- livinglabstack/build_llab_stack.sh
+++ livinglabstack/build_llab_stack.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 curl https://raw.githubusercontent.com/edinburghlivinglab/dds-notebooks/master/requirements.txt > requirements.txt
+curl https://raw.githubusercontent.com/edinburghlivinglab/dds-notebooks/master/extra_requirements.txt > extra_requirements.txt
 curl https://raw.githubusercontent.com/CamDavidsonPilon/Probabilistic-Programming-and-Bayesian-Methods-for-Hackers/master/requirements.txt > ppbmh_requirements.txt
 
 # have to remove certain packages
@@ -19,6 +20,8 @@ for PYTHONVER in 2 3 ; do
   $PIP install --upgrade pip
   # Installing living lab requirements
   $PIP install -r $REQ
+  # Install more requirements, just in case we need them later
+  $PIP install -r extra_requirements.txt
   # and install extra requirements (have to do this due to wsgiref)
   cat ppbmh_requirements.txt | grep -v wsgiref | xargs $PIP install
   $PIP install scikit-learn
