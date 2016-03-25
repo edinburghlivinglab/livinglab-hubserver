c51898f43f38bb62258ec9fed1a3200e61cd900d
Gavin <g.d.b.gray@sms.ed.ac.uk>
2016-03-25 17:26:16 +0000

    builds, should be better base image

--- livinglabstack/build_llab_stack.sh
+++ livinglabstack/build_llab_stack.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-curl https://raw.githubusercontent.com/edinburghlivinglab/dds-notebooks/master/requirements.txt > requirements.txt
-curl https://raw.githubusercontent.com/CamDavidsonPilon/Probabilistic-Programming-and-Bayesian-Methods-for-Hackers/master/requirements.txt > ppbmh_requirements.txt
-
 # have to remove certain packages
 cat requirements.txt | grep -v jupyterhub > requirements.txt.2
 cp requirements.txt requirements.txt.3
-cat ppbmh_requirements.txt | grep -v pymc > ppbmh_requirements.nopymc.txt
+cat ppbmh_requirements.txt | grep -v pymc | grep -v wsgiref > ppbmh_requirements.nopymc.txt
 
 # clone in dds lab
 git clone https://github.com/edinburghlivinglab/dds-notebooks.git
-cd dds-notebooks
 
 # install for Python 3
 conda install shapely
 pip install -r requirements.txt.3
 conda install pymc
 pip install -r ppbmh_requirements.nopymc.txt
+cd dds-notebooks
 pip install --no-deps -e .
+cd ..
 
 # install for Python 2
 source activate python2
@@ -26,4 +24,6 @@ conda install shapely
 pip install -r requirements.txt.2
 conda install pymc
 pip install -r ppbmh_requirements.nopymc.txt
+cd dds-notebooks
 pip install --no-deps -e .
+cd ..
