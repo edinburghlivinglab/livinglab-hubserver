625ed941163fb4ec20fb934c71ed900725c2a35b
Gavin <gavingray1729@gmail.com>
2016-03-25 16:25:52 +0000

    updating to non-deprecated base

--- livinglabstack/build_llab_stack.sh
+++ livinglabstack/build_llab_stack.sh
@@ -5,31 +5,25 @@ curl https://raw.githubusercontent.com/edinburghlivinglab/dds-notebooks/master/r
 curl https://raw.githubusercontent.com/CamDavidsonPilon/Probabilistic-Programming-and-Bayesian-Methods-for-Hackers/master/requirements.txt > ppbmh_requirements.txt
 
 # have to remove certain packages
-cp requirements.txt requirements.txt.backup
-cat requirements.txt.backup | grep -v igraph > requirements.txt
 cat requirements.txt | grep -v jupyterhub > requirements.txt.2
 cp requirements.txt requirements.txt.3
+cat ppbmh_requirements.txt | grep -v pymc > ppbmh_requirements.nopymc.txt
 
 # clone in dds lab
 git clone https://github.com/edinburghlivinglab/dds-notebooks.git
+cd dds-notebooks
 
-for PYTHONVER in 2 3 ; do
-  PIP="pip$PYTHONVER"
-  PYTHON="python$PYTHONVER"
-  REQ="requirements.txt.$PYTHONVER"
-  $PIP install --upgrade pip
-  # Installing living lab requirements
-  $PIP install -r $REQ
-  # and install extra requirements (have to do this due to wsgiref)
-  cat ppbmh_requirements.txt | grep -v wsgiref | xargs $PIP install
-  $PIP install scikit-learn
-  $PIP install nltk
-  # finally install dds_notebooks
-  cd dds-notebooks
-  $PIP install --no-deps -e .
-  cd ..
-done
+# install for Python 3
+conda install shapely
+pip install -r requirements.txt.3
+conda install pymc
+pip install -r ppbmh_requirements.nopymc.txt
+pip install --no-deps -e .
 
-# Reduce the image size
-apt-get autoremove -y
-apt-get clean -y
+# install for Python 2
+source activate python2
+conda install shapely
+pip install -r requirements.txt.2
+conda install pymc
+pip install -r ppbmh_requirements.nopymc.txt
+pip install --no-deps -e .
